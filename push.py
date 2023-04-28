303943a6d31e48d15e62ea2f6f66bbc25f159efb
rzs <site.administrator@rzsgymn.ukr.education>
2023-04-28 14:58:25 +0300

    add first news

--- push.py
+++ push.py
@@ -2,11 +2,13 @@ import os
 
 
 def git_push():
-    print('hello')
+    print('***hello***')
     os.system('git status')
+    print('***hello***')
     os.system('git add .')
+    print('***hello***')
     os.system('git commit -m "add first news"')
-    os.system('git push')
+    # os.system('git push')
 
 
 git_push()
